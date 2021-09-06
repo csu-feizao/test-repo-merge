e9659375cb03eaed3acab78daca077ba621a8978
Xinghua Hong <hongxinghua@qiniu.com>
2021-09-06 20:21:08 +0800

    done

---
+++ merge/apps/landing/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Thing } from 'components';
+
+function App() {
+    return (
+        <div className="Apsp">
+        <Thing />
+        </div>
+);
+}
+
+export default App;
